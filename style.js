649657719d99943799f4350b02a5e4057c2f1f9c
IW <122602491+FadeIgor@users.noreply.github.com>
2024-11-28 11:23:15 +0100

    Update style.jsdwasdad

--- style.js
+++ style.js
@@ -34,6 +34,37 @@ const imgs = Array.from(document.images)
 const imgs1 = [...document.write]
 
 /*
+3. dwie metody pobierajace 1 element
+document powoluje sie do calej strtony ale my chcemy odwolac sie do konkretnego
+A. Metoda querySelector                                                         */
+document.querySelector("li:nth-child(3")
+
+//B metoda getElementById
+const firstElement = document.getElementById('first')
+//gdybym chcial w js za pomoca qutrySelector() pobrac to co mam w nawiasach to nie moge wpisac ("first"), ale musialbyum zapisac ("#first")
+
+const firstElement = document.querySelector("#first")
+
+//jesli nie znajde jakiegos elementru do dostane null i nei ma bledu
+document.querySelector("li:nth-child(6")
+
+/*
+4. METODY POBIERAJACE WSZYSTKIE PASUJACE ELEMENTRY A NIE TYLKO JEDEN
+- praca z jednym elementem jest latwiejsza - bo ma pewne metody i wlasciwosci
+- jak pobieerzemy wielee elementow to sa one umieszczone w obiekcie tablico podobnym ktora musimy zmienic na tablice w tedy jednak bedziemy musieli wykonac albo petle albo jakas metode aby do kazdego elementu sie dostac
+WNIOSEK inaczej pracuje kiedy mamy 1 element pobrany a inaczej jak mam wiele elementow - tak jak ponizej
+
+querySelector() - metoda do pobrania 1 elementu 
+
+querySelectorAll() - do wielu elementow
+
+*/
+const  list = document.quetySelectorAll("ul>li");
+/*
+pobieram wszytkie li ktore sa dzieckiem ul
+zwrocona zostanie tu NodeList - czyli lista wezlow obiekt tablico podobny
+ktory ma dlugosc length i metody tablicy, ale mniej niz tablica
+
 II. ------------------Modyfikowanie elementow na stronie internetowej------------------
 Mam duzo metod i wlasciowosci, ktore umozliwiaja mi modyfikacje tego co widze na stronie internetoej 
 
